4714fa87bc090e8074f6c4e1167c407a81b45087
Frank <frankz@eyebuydirect.com>
2017-08-07 18:36:14 +0800

    add some

---
+++ notes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="src/css/bootstrap.min.css" rel="stylesheet">
+<script src="src/js/markdown.min.js"></script>
+</head>
+<body style="padding:30px">
+<div><a href='/'>返回首页</a></div>
